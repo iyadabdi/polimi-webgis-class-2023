0989d149b23ef56bec5d782274ca8aaa6a08b9d9
Your Name <125884998+iyadabdi@users.noreply.github.com>
2023-06-22 14:28:57 +0200

    webgis popup

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,6 +7,19 @@ let osm = new ol.layer.Tile({
     source: new ol.source.OSM()
 });
 
+//Step *: Add suceptibility clip Valle Imagna
+
+var Reclass_resampled_susceptibility_map = new ol.layer.Image({
+  title: "Suceptibility map",
+  source: new ol.source.ImageWMS({
+      url: 'https://www.gis-geoserver.polimi.it/geoserver/gisgeoserver_05/wms?service=WMS&version=1.1.0&request=GetMap&layers=gisgeoserver_05%3AReclass_resampled_susceptibility_map&bbox=536399.205%2C5066963.4401%2C547016.1198%2C5079867.0751&width=631&height=768&srs=EPSG%3A32632&styles=&format=application/openlayers',
+      params: { 'LAYERS': 'Reclass_resampled_susceptibility_map', 'STYLES': 'suceptibility'}
+  }),
+  opacity: 1,
+  visible: true  // Set the layer to be visible by default
+});
+
+
 //Step *: Add Population clip Valle Imagna
 var Worldpop_clipped = new ol.layer.Image({
     title: "Population",
@@ -17,6 +30,33 @@ var Worldpop_clipped = new ol.layer.Image({
     opacity: 1,
     visible: false  // Set the layer to be visible by default
 });
+
+//Step *: Add ls inventory
+var vectorSource = new ol.source.Vector({});
+
+function loadFeatures(response) {
+  vectorSource.addFeatures(new ol.format.GeoJSON().readFeatures(response));
+}
+
+var base_url = "https://www.gis-geoserver.polimi.it/geoserver/gisgeoserver_05/ows?";
+var wfs_url = base_url;
+wfs_url += "service=WFS&";
+wfs_url += "version=2.0.0&";
+wfs_url += "request=GetFeature&";
+wfs_url += "typeName=gisgeoserver_05%3ALS_inventory&";
+wfs_url += "outputFormat=text%2Fjavascript&";
+wfs_url += "srsname=EPSG:3857&";
+wfs_url += "format_options=callback:loadFeatures";
+
+$.ajax({url: wfs_url, dataType: 'jsonp'});
+
+var LS_inventory = new ol.layer.Vector({
+  title: "LS inventory",
+  source: vectorSource,
+  visible: true
+});
+
+
 
 //Step *: Add aspect clip Valle Imagna
 var aspect = new ol.layer.Image({
@@ -118,16 +158,27 @@ var roads = new ol.layer.Image({
     visible: false  // Set the layer to be visible by default
 });
 
-//Step *: Add suceptibility clip Valle Imagna
-var Reclass_resampled_susceptibility_map = new ol.layer.Image({
-    title: "Suceptibility map",
+//Add test and train points
+var Testing_points_sampled = new ol.layer.Image({
+    title: "Testing points",
     source: new ol.source.ImageWMS({
-        url: 'https://www.gis-geoserver.polimi.it/geoserver/gisgeoserver_05/wms?service=WMS&version=1.1.0&request=GetMap&layers=gisgeoserver_05%3AReclass_resampled_susceptibility_map&bbox=536399.205%2C5066963.4401%2C547016.1198%2C5079867.0751&width=631&height=768&srs=EPSG%3A32632&styles=&format=application/openlayers',
-        params: { 'LAYERS': 'Reclass_resampled_susceptibility_map', 'STYLES': 'suceptibility'}
+        url: 'https://www.gis-geoserver.polimi.it/geoserver/gisgeoserver_05/wms?service=WMS&version=1.1.0&request=GetMap&layers=gisgeoserver_05%3ATesting_points_sampled&bbox=537049.767596203%2C5068136.9723774195%2C546214.2056630501%2C5079606.03210117&width=613&height=768&srs=EPSG%3A32632&styles=&format=application/openlayers',
+        params: { 'LAYERS': 'Testing_points_sampled', 'STYLES': 'testing_pt'}
     }),
     opacity: 1,
-    visible: true  // Set the layer to be visible by default
+    visible: false
 });
+var Training_points_sampled = new ol.layer.Image({
+    title: "Training points",
+    source: new ol.source.ImageWMS({
+        url: 'https://www.gis-geoserver.polimi.it/geoserver/gisgeoserver_05/wms?service=WMS&version=1.1.0&request=GetMap&layers=gisgeoserver_05%3ATraining_points_sampled&bbox=537050.4892907584%2C5067069.2166941855%2C546645.4743933523%2C5079667.042552076&width=584&height=768&srs=EPSG%3A32632&styles=&format=application/openlayers',
+        params: { 'LAYERS': 'Training_points_sampled', 'STYLES': 'training_pt'}
+    }),
+    opacity: 1,
+    visible: false
+});
+
+
 
 
 //Step *: AGGIUNGERE TUTTI GLI ALTRI COME FATTO SOPRA
@@ -138,15 +189,18 @@ let basemapLayers = new ol.layer.Group({
     layers: [osm]
 });
 
-let Analytical_input = new ol.layer.Group({  //AGGIORNARE IL NOME DEL GRUPPO CON QUALCOSA PIU CARINO
-    title: "Analytical input",
-    layers: [Worldpop_clipped, aspect, dtm, dusaf, faults, ndvi, plan, profile, rivers, roads]    //POPOLARE [] CON I NUOVI LAYER AGGIUNTI
-});
-
 let Analytical_output = new ol.layer.Group({  //AGGIORNARE IL NOME DEL GRUPPO CON QUALCOSA PIU CARINO
     title: "Analytical output",
     layers: [Reclass_resampled_susceptibility_map]    //POPOLARE [] CON I NUOVI LAYER AGGIUNTI
 });
+
+let Analytical_input = new ol.layer.Group({  //AGGIORNARE IL NOME DEL GRUPPO CON QUALCOSA PIU CARINO
+    title: "Analytical input",
+    layers: [LS_inventory, Worldpop_clipped, aspect, dtm, dusaf, faults, ndvi, plan, profile, rivers, roads, Testing_points_sampled, Training_points_sampled]    //POPOLARE [] CON I NUOVI LAYER AGGIUNTI
+});
+
+
+
 
 //Put here the main map object.
 const initialZoom = 12.2;
@@ -220,3 +274,94 @@ var stamenToner = new ol.layer.Tile({
 });
 //Get the list of basemaps and Extend the list using the .extend() function adding the 2 new layers
 basemapLayers.getLayers().extend([stamenterrain, stamenToner]);
+
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+
+var popup = new ol.Overlay({
+  element: container
+});
+
+map.addOverlay(popup);
+
+//map.on('singleclick', function(event) {
+  //var coord = map.getCoordinateFromPixel(event.pixel);
+  //popup.setPosition(coord);
+  //content.innerHTML = '<h5>Test Popup</h5><p>This is a test popup.</p>';
+//});
+
+
+
+
+
+closer.onclick = function() {
+  popup.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
+map.on('pointermove', function(event) {
+  var pixel = map.getEventPixel(event.originalEvent);
+  var hit = map.hasFeatureAtPixel(pixel);
+  map.getTargetElement().style.cursor = hit ? 'pointer' : '';
+});
+
+
+//////////////////// original below
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+
+var popup = new ol.Overlay({
+  element: container
+});
+
+map.addOverlay(popup);
+
+map.on('singleclick', function(event) {
+  if (Reclass_resampled_susceptibility_map.getVisible()) {
+    var viewResolution = map.getView().getResolution();
+    var url = Reclass_resampled_susceptibility_map.getSource().getFeatureInfoUrl(
+      event.coordinate,
+      viewResolution,
+      'EPSG:3857',
+      {'INFO_FORMAT': 'application/json'}
+    );
+
+    if (url) {
+      fetch(url)
+        .then(function(response) { return response.json(); })
+        .then(function(json) {
+          var result = json.features[0].properties;
+          var pixel_value = result['GRAY_INDEX'];
+
+          var pixel = map.getPixelFromCoordinate(event.coordinate);
+          popup.setPosition(event.coordinate);
+          content.innerHTML = '<h5>Susceptibility Map</h5><b>Risk level: </b>' + pixel_value;
+        });
+    }
+  } else if (LS_inventory.getVisible()) {
+    var feature = map.forEachFeatureAtPixel(event.pixel, function(feature, layer) {
+      return feature;
+    });
+    if (feature != null) {
+      var pixel = event.pixel;
+      var coord = map.getCoordinateFromPixel(pixel);
+      popup.setPosition(coord);
+      content.innerHTML = '<h5>Landslide Inventory</h5>' + '<b>Description: </b>' + feature.get('TIPOLOGIA');
+    }
+  }
+});
+
+closer.onclick = function() {
+  popup.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
+map.on('pointermove', function(event) {  // Corrected event name from pointmove to pointermove
+  var pixel = map.getEventPixel(event.originalEvent);
+  var hit = map.hasFeatureAtPixel(pixel);
+  map.getTargetElement().style.cursor = hit ? 'pointer' : '';
+});
